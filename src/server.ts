b079a1898f4f96b3ce61ec0a31b9b45e933d954c
Ben ROUVIERE <rouviere.benjamin@gmail.com>
2020-09-04 12:38:50 +0200

    first profile routes (get & post)

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,8 @@ import express, { Request, Response, ErrorRequestHandler } from 'express';
 import morgan from "morgan";
 import helmet from "helmet";
 import { configuration, IConfig } from "./config";
+import { Profile } from './models/Profiles';
+import { connect } from './database';
 
 export function createExpressApp(config: IConfig): express.Express {
   const { express_debug } = config;
@@ -18,6 +20,24 @@ export function createExpressApp(config: IConfig): express.Express {
   }) as ErrorRequestHandler);
 
   app.get('/', (req: Request, res: Response) => { res.send('This is the boilerplate for Flint Messenger app') });
+  app.get('/profile/:id', (req: Request, res: Response) => { 
+    const profileId = req.params['id'];
+
+    Profile.findById(profileId, '_id email', (err, profile) => {
+      if(err) { console.log('Woops, error'); }
+      if(profile == null) { console.log('No profile found') }
+
+      res.send(profile);
+    })
+  });
+
+  app.post('/profile', (req: Request, res: Response) => {
+    const {email, firstname, lastname} = req.body;
+
+    const newProfile = new Profile({email: email, firstname: firstname, lastname: lastname})
+    newProfile.save();
+    res.status(200).send('User created !');
+  })
 
   return app;
 }
@@ -25,4 +45,7 @@ export function createExpressApp(config: IConfig): express.Express {
 const config = configuration();
 const { PORT } = config;
 const app = createExpressApp(config);
-app.listen(PORT, () => console.log(`Flint messenger listening at ${PORT}`));
+connect(config).then( () => {
+  app.listen(PORT, () => console.log(`Flint messenger listening at ${PORT}`));
+})
+
