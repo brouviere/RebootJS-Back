3c0012672a26d9fd4b0c42cbc47a7b21d6d572f8
Ben ROUVIERE <rouviere.benjamin@gmail.com>
2020-09-04 11:15:06 +0200

    connect mongo cloud database

---
+++ export-pwd.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+export mongo_pass=qBh3TioopbNrWzIM
--- src/config.ts
+++ src/config.ts
@@ -8,6 +8,10 @@ const defaultConfig = JSON.parse(readFileSync(resolve(__dirname, '../config.json
 export interface IConfig {
   PORT: number;
   express_debug: boolean;
+  mongo_host: string,
+  mongo_user: string,
+  mongo_pass: string,
+  mongo_database: string
 }
 
 export function configuration(): IConfig {
